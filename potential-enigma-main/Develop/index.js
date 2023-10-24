01edc45f41407f11929d122585afe6452fe6f2bb
gracecatk <gracie.kiker@gmail.com>
2023-10-23 22:29:17 -0500

    added more readme sections

--- potential-enigma-main/Develop/index.js
+++ potential-enigma-main/Develop/index.js
@@ -8,18 +8,47 @@ const generateMarkdown = require('./utils/generateMarkdown'); // Import the gene
 const questions = [
   {
     type: 'input',
-    name: 'title',
+    name: 'Title',
     message: 'Enter the title of your project: ',
   },
   {
     type: 'input',
-    name: 'description',
+    name: 'Description',
     message: 'Provide a brief description of your project: ',
   },
-  // Add more questions for user input as needed
+  {
+    type: 'input',
+    name: 'Table of contents',
+    message: 'Provide a table of contents for your project: ',
+  },
+  {
+    type: 'input',
+    name: 'Installations',
+    message: 'Provide a brief description of your project: ',
+  },
+  {
+    type: 'input',
+    name: 'Usage',
+    message: 'Provide a brief description of your project: ',
+  },
+  {
+    type: 'input',
+    name: 'Contributing',
+    message: 'Provide a brief description of your project: ',
+  },
+  {
+    type: 'input',
+    name: 'Tests',
+    message: 'Provide a brief description of your project: ',
+  },
+  {
+    type: 'input',
+    name: 'Questions',
+    message: 'Provide a brief description of your project: ',
+  },
 ];
 
-// TODO: Create a function to write README file
+// TODO: Create a function to write README file- a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 function writeToFile(fileName, data) {
   // Use the fs module to write the data to a file with the given filename
   fs.writeFile(fileName, data, (err) => {
