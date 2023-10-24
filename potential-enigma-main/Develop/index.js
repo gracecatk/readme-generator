a69b37585798ba5a768d8e95d465187b47f20f10
gracecatk <gracie.kiker@gmail.com>
2023-10-23 22:16:27 -0500

    fixing bugs

--- potential-enigma-main/Develop/index.js
+++ potential-enigma-main/Develop/index.js
@@ -1,46 +1,47 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkdown = require('./generateMarkdown'); // Assuming 'generateMarkdown.js' is in the same directory
+const fs = require('fs'); // For file system operations
+const inquirer = require('inquirer'); // For user prompts
 
 // TODO: Create an array of questions for user input
 const questions = [
   {
     type: 'input',
     name: 'title',
-    message: 'Enter the title of your project:',
+    message: 'Enter the title of your project: ',
   },
   {
     type: 'input',
     name: 'description',
-    message: 'Provide a description of your project:',
+    message: 'Provide a brief description of your project: ',
   },
-  // TODO: Add more questions for other project details (installation, usage, license, etc.)
+  // Add more questions for user input as needed
 ];
 
-//TODO: Create a function to write the README file
+// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+  // Use the fs module to write the data to a file with the given filename
   fs.writeFile(fileName, data, (err) => {
     if (err) {
-      console.log(err);
+      console.error(err);
     } else {
-      console.log('README.md has been successfully generated!');
+      console.log('README.md has been created successfully!');
     }
   });
 }
 
-// TODO: Create a function to initialize the app
+// TODO: Create a function to initialize app
 function init() {
-  inquirer
-    .prompt(questions)
-    .then((userResponses) => {
-      const markdown = generateMarkdown(userResponses);
-      writeToFile('README.md', markdown);
-    })
-    .catch((error) => {
-      console.error('Error occurred during inquirer prompts:', error);
-    });
+  inquirer.prompt(questions).then((answers) => {
+    // Generate the README content using the answers
+    const readmeContent = generateMarkdown(answers);
+
+    // Specify the filename for the README
+    const fileName = 'README.md';
+
+    // Write the README file
+    writeToFile(fileName, readmeContent);
+  });
 }
 
-// Function call to initialize the app
+// Function call to initialize app
 init();
--- potential-enigma-main/Develop/utils/generateMarkdown.js
+++ potential-enigma-main/Develop/utils/generateMarkdown.js
@@ -1,79 +1,39 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license) {
-    // You can define badge URLs for different licenses here
-    // For example, if you're using shields.io for badges:
-    // const badgeURL = `https://img.shields.io/badge/license-${license}-brightgreen.svg`;
-    // Or use other sources for badges
-    return `[![License](badgeURL)](licenseURL)`;
+  switch (license) {
+    case 'MIT':
+      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    case 'GNU GPLv3':
+      return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+    // Add more cases for other licenses as needed
+    default:
+      return '';
   }
-  return ''; // Return an empty string if no license is provided
 }
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license) {
-    // You can provide links to the full text of licenses or other relevant information
-    // For example, if you have license URLs:
-    // const licenseURL = 'https://www.example.com/license';
-    return `[License](licenseURL)`;
+  switch (license) {
+    case 'MIT':
+      return '[MIT License](https://opensource.org/licenses/MIT)';
+    case 'GNU GPLv3':
+      return '[GNU GPLv3 License](https://www.gnu.org/licenses/gpl-3.0)';
+    // Add more cases for other licenses as needed
+    default:
+      return '';
   }
-  return ''; // Return an empty string if no license is provided
 }
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license) {
-    // You can provide a brief description or summary of the license here
-    return `This project is licensed under the [${license} License](licenseURL).`;
+    return `This project is licensed under the [${license} License](${renderLicenseLink(license)}).`;
+  } else {
+    return ''; // No license provided
   }
-  return ''; // Return an empty string if no license is provided
 }
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  const licenseBadge = renderLicenseBadge(data.license);
-  const licenseSection = renderLicenseSection(data.license);
-
   return `# ${data.title}
-
-${licenseBadge}
-
+${renderLicenseBadge(data.license)}
 ## Description
 ${data.description}
-
-## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [License](#license)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [Questions](#questions)
-
-## Installation
-${data.installation}
-
-## Usage
-${data.usage}
-
 ## License
-${licenseSection}
-
-## Contributing
-${data.contributing}
-
-## Tests
-${data.tests}
-
-## Questions
-If you have any questions, please contact ${data.email}.
-
-GitHub: [${data.github}](https://github.com/${data.github})
+${renderLicenseSection(data.license)}
 `;
-
 }
-
 module.exports = generateMarkdown;
