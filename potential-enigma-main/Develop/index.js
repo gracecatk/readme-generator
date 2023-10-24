b0821a58d0369f043f797367245861f44d606af5
gracecatk <gracie.kiker@gmail.com>
2023-10-23 22:11:00 -0500

    fixed markdown & index.js

--- potential-enigma-main/Develop/index.js
+++ potential-enigma-main/Develop/index.js
@@ -1,13 +1,46 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown'); // Assuming 'generateMarkdown.js' is in the same directory
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter the title of your project:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  // TODO: Add more questions for other project details (installation, usage, license, etc.)
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//TODO: Create a function to write the README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('README.md has been successfully generated!');
+    }
+  });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// TODO: Create a function to initialize the app
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((userResponses) => {
+      const markdown = generateMarkdown(userResponses);
+      writeToFile('README.md', markdown);
+    })
+    .catch((error) => {
+      console.error('Error occurred during inquirer prompts:', error);
+    });
+}
 
-// Function call to initialize app
+// Function call to initialize the app
 init();
--- potential-enigma-main/Develop/utils/generateMarkdown.js
+++ potential-enigma-main/Develop/utils/generateMarkdown.js
@@ -1,20 +1,79 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+    // You can define badge URLs for different licenses here
+    // For example, if you're using shields.io for badges:
+    // const badgeURL = `https://img.shields.io/badge/license-${license}-brightgreen.svg`;
+    // Or use other sources for badges
+    return `[![License](badgeURL)](licenseURL)`;
+  }
+  return ''; // Return an empty string if no license is provided
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+    // You can provide links to the full text of licenses or other relevant information
+    // For example, if you have license URLs:
+    // const licenseURL = 'https://www.example.com/license';
+    return `[License](licenseURL)`;
+  }
+  return ''; // Return an empty string if no license is provided
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    // You can provide a brief description or summary of the license here
+    return `This project is licensed under the [${license} License](licenseURL).`;
+  }
+  return ''; // Return an empty string if no license is provided
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+
   return `# ${data.title}
 
+${licenseBadge}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${licenseSection}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, please contact ${data.email}.
+
+GitHub: [${data.github}](https://github.com/${data.github})
 `;
+
 }
 
 module.exports = generateMarkdown;
